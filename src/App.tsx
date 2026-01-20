6f58a51437eac64fe8f638f7db11850d7dd19e74
anderslu26 <anderslu26@student.tcs.act.edu.au>
2026-01-21 09:02:50 +1100

    Test 5

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <p>Test Text 4</p>
+      <p>Test Text 5</p>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
